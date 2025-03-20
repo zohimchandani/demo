2d7e2296e972a49cafa7ac6f5fd764c2a81d1b3e
Zohim Chandani GB <zchandani@nvidia.com>
2025-03-20 18:09:16 +0000

    updates

--- hello_world.py
+++ hello_world.py
@@ -1,20 +1,18 @@
+import os 
+os.environ["CUDAQ_MAX_CPU_MEMORY_GB"] = "NONE"
+print("CUDAQ_MAX_CPU_MEMORY_GB:", os.environ.get("CUDAQ_MAX_CPU_MEMORY_GB"))
+
 import cudaq
+
 cudaq.set_target('nvidia', option='mgpu,fp32')
 
 # cudaq.set_target('nvidia')
-
-# import os 
-# os.environ["CUDAQ_MAX_CPU_MEMORY_GB"] = "NONE"
-# os.environ["CUDAQ_MAX_GPU_MEMORY_GB"] = "NONE"
-# print("CUDAQ_MAX_CPU_MEMORY_GB:", os.environ.get("CUDAQ_MAX_CPU_MEMORY_GB"))
-# print("CUDAQ_MAX_GPU_MEMORY_GB:", os.environ.get("CUDAQ_MAX_GPU_MEMORY_GB"))
-
 
 cudaq.mpi.initialize()
 
 print('mpi initialized?', cudaq.mpi.is_initialized())
 
-n_qubits = 34
+n_qubits = 35
 
 @cudaq.kernel
 def kernel(n_qubits:int):
@@ -27,7 +25,8 @@ def kernel(n_qubits:int):
 
 print('starting exp val calculation')
 
-expectation_value = cudaq.observe(kernel, cudaq.spin.z(0), n_qubits).expectation()
+expectation_value = cudaq.observe(kernel, cudaq.spin.z(0), n_qubits)
+# expectation_value = cudaq.sample(kernel, n_qubits)
 
 print("num qubits", n_qubits, 'expectation_value', expectation_value)
 
